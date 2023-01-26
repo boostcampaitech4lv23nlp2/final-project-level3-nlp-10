2a358a1f3f048fb531d46f03f698024522d6ab3b
mountain96 <jtlsan@naver.com>
2023-01-26 15:42:08 +0000

    feat: Add argumentparser to main.py

--- ml/main.py
+++ ml/main.py
@@ -1,18 +1,33 @@
-# import sys
-# from model.dpr_eval import eval as dpr_eval
-# from model.dpr_train import train as dpr_train
+import argparse
+import sys
+
+import torch
+from model.dpr_eval import eval as dpr_eval
+from model.dpr_train import train as dpr_train
 from model.fid_train import train as fid_train
 from omegaconf import OmegaConf
 
+
+def get_args():
+    arg_parser = argparse.ArgumentParser()
+    arg_parser.add_argument("--option", required=True, choices=["dpr", "fid"], help="어떤 방식의 모델을 돌릴지 선택")
+    arg_parser.add_argument("--type", default="train", choices=["train", "eval", "predict"], help="모델을 어떤 형식으로 돌릴지 선택")
+
+
 if __name__ == "__main__":
-    """
-    cmd = sys.argv[1]
-    if len(sys.argv) > 2:
-        args = sys.argv[2:]
-    if cmd == 'dpr_train':
-        dpr_train()
-    """
+    if not torch.cuda.is_available():
+        print("Our program supports only CUDA enabled machines")
+        sys.exit(1)
+    sys_args = get_args()
     conf = OmegaConf.load("./config.yaml")
-    # dpr_train(conf)
-    # dpr_train()
-    fid_train(conf)
+
+    if sys_args.option == "dpr":
+        if sys_args.type == "train":
+            dpr_train(conf)
+        else:
+            dpr_eval(conf)
+    elif sys_args.option == "fid":
+        if sys_args.type == "train":
+            fid_train(conf)
+        else:
+            pass
