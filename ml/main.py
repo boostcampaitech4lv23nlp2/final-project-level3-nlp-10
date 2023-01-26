7ca7c3986809f537b5613b9560637b61233d6f81
mountain96 <jtlsan@naver.com>
2023-01-26 15:46:43 +0000

    feat: Add function to set seed in main.py

--- ml/main.py
+++ ml/main.py
@@ -1,6 +1,8 @@
 import argparse
+import random
 import sys
 
+import numpy as np
 import torch
 from model.dpr_eval import eval as dpr_eval
 from model.dpr_train import train as dpr_train
@@ -14,12 +16,20 @@ def get_args():
     arg_parser.add_argument("--type", default="train", choices=["train", "eval", "predict"], help="모델을 어떤 형식으로 돌릴지 선택")
 
 
+def set_seed(seed):
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+    np.random.seed(seed)
+    random.seed(seed)
+
+
 if __name__ == "__main__":
     if not torch.cuda.is_available():
         print("Our program supports only CUDA enabled machines")
         sys.exit(1)
     sys_args = get_args()
     conf = OmegaConf.load("./config.yaml")
+    set_seed(conf.common.seed)
 
     if sys_args.option == "dpr":
         if sys_args.type == "train":
