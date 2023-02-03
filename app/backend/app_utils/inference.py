250d098bb34ecb221eabe611145e67f73a3e3aed
mountinyy <jtlsan1996@gmail.com>
2023-02-03 12:09:29 +0000

    Update generate function to use beam search

--- app/backend/app_utils/inference.py
+++ app/backend/app_utils/inference.py
@@ -7,27 +7,28 @@ from omegaconf import OmegaConf
 conf = OmegaConf.load("./config.yaml")
 
 
-def summarize_fid(keys: List[str], debug=False):
+def summarize_fid(keys: List[str], debug=False, renew_emb=True):
     (retriever_tokenizer, reader_tokenizer) = load_tokenizer()
     retriever = load_retriever()
     model = load_model(model_type="fid")
-    retriever
-    model
     query = retriever_tokenizer.sep_token.join(keys)
     tokenized_query = retriever_tokenizer(
         [query], padding="max_length", truncation=True, max_length=512, return_tensors="pt"
     )
 
-    top_docs = get_top_docs(retriever, tokenized_query)[0]
+    top_docs = get_top_docs(retriever, tokenized_query, renew_emb=renew_emb)[0]
 
     input = [f"{reader_tokenizer.bos_token}질문: {query} 문서: {doc}{reader_tokenizer.eos_token}" for doc in top_docs]
     tokenized_input = reader_tokenizer(
         input, padding="max_length", truncation=True, max_length=512, return_tensors="pt"
     )
+
     output = model.generate(
         input_ids=tokenized_input["input_ids"].unsqueeze(0),
         attention_mask=tokenized_input["attention_mask"].unsqueeze(0),
         max_length=512,
+        num_beams=5,
+        early_stopping=True,
     )[0]
     output = reader_tokenizer.decode(output, skip_special_tokens=True)
     if debug:
@@ -37,11 +38,10 @@ def summarize_fid(keys: List[str], debug=False):
     return output
 
 
-def get_top_docs(retriever, query):
-    create_context_embedding("resources/meeting_records/output.json")
+def get_top_docs(retriever, query, renew_emb=True):
+    create_context_embedding("resources/meeting_records/output.json", renew_emb=renew_emb)
     # top_doc_indices: (1, topk) tensor
     top_doc_indices = retriever.get_relevant_doc(query, k=conf.topk)
-    print(top_doc_indices)
     # top_docs: (1, topk) List
     top_docs = retriever.get_passage_by_indices(top_doc_indices)
     return top_docs
