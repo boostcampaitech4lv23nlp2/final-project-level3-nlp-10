8f8d1617d007ac53a23ba92c98f29dda61570184
Jlnus <zmdk0902@naver.com>
2023-02-07 05:55:46 +0000

    feat: fix input words

--- app/fronted/Boost2Note.py
+++ app/fronted/Boost2Note.py
@@ -114,8 +114,10 @@ with con2:
             st.session_state["keywords"],
         )
         input_keywords = st_tags(label="키워드 직접 입력", text="Press enter to add more")
-        keywords_set = set(options + [i.strip(" ") for i in input_keywords if i != ""])
+        st.write(input_keywords)
+        keywords_set = list(set(options + [i.strip(" ") for i in input_keywords if i != " "]))
         # 띄어쓰기 제거 및 중복 제거
+
     else:
         options = st.multiselect(
             "주요 키워드",
@@ -132,6 +134,6 @@ with con2:
 
     with con8:
         if st.button("요약하기", key="summarization"):
-            response = requests.post("http://localhost:8000/summarize", json={"keywords": (keywords_set)})
+            response = requests.post("http://localhost:8000/summarize", json={"keywords": keywords_set})
             json_res = json.loads(response.text)  # json_res : list
             call(con2, expanders, json_res)
